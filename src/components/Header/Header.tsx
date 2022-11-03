cb95889b0cc4a11b228261b8e78d764a1bc967c0
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-03 07:12:12 -0400

    Add style a Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,37 @@
 
-import bgCardFront from '../../assets/images/bg-card-front.png'
+import logo from '../../assets/images/card-logo.svg'
 import bgCardBack from "../../assets/images/bg-card-back.png";
 import type { cardType } from '../../type';
 
 import './Header.scss'
 
 type Props = {
-    card:cardType
+  card: cardType
 }
 
-const Header = ({card}:Props) => {
+const Header = ({ card }: Props) => {
   return (
     <header className="header">
-    <div className="header__cardfront">
-      <img src={bgCardFront} alt="Card Front" />
-      <p className="header__cardfront__number">
-        {card.number === '' ? "0000 0000 0000 0000" : card.number}
-      </p>
-      <p className="header__cardfront__name">
-        {card.name === "" ? "Jane Appleseed" : card.name}
-      </p>
-      <p className="header__cardfront__yy-mm">
-        {card.yy === 0 ? "00" : card.yy}/{card.mm === 0 ? "00" : card.mm}
-      </p>
-    </div>
-    <div className="header__cardback">
-      <img src={bgCardBack} alt="Card back" />
-      <p className="header__cardback__cvc">
-        {card.cvc === 0 ? "000" : card.cvc}
-      </p>
-    </div>
-  </header>
+      <div className="header__cardfront">
+        <img className='header__cardfront__logo' src={logo} alt="Card Logo" />
+        <p className="header__cardfront__number">
+          {card.number === '' ? "0000 0000 0000 0000" : card.number}
+        </p>
+        <div className='header__cardfront__details'>
+          <p className="header__cardfront__details-name">
+            {card.name === "" ? "Jane Appleseed" : card.name}
+          </p>
+          <p className="header__cardfront__details-date">
+            <span className="header__cardfront__details-date-mm">{card.yy === 0 ? "00" : card.yy}/</span><span className="header__cardfront__details-date-yy">{card.mm === 0 ? "00" : card.mm}</span>
+          </p>
+        </div>
+      </div>
+      <div className="header__cardback">
+        <p className="header__cardback__cvc">
+          {card.cvc === 0 ? "000" : card.cvc}
+        </p>
+      </div>
+    </header>
   )
 }
 
