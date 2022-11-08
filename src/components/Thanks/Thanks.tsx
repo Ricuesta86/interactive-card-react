b13bdd04bb18cd7924368367dbf024ebfa9b5064
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-08 07:08:05 -0500

    Working with the styles Thanks component

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -7,8 +7,8 @@ const Thanks = ({handleShow}:Props) => {
   return (
     <div className='thanks'>
         <img  className='thanks__img'src={complete} alt="Icon" />
-        <h1 className='thanks__h1'>Thank You!</h1>
-        <p className='thanks__p'>We'ne added your card details</p>
+        <h1 className='thanks__title'>Thank You!</h1>
+        <p className='thanks__text'>We'ne added your card details</p>
         <button className='thanks__btn' onClick={handleShow}>Continue</button>
     </div>
   )
